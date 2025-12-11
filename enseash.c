9b162dac448f6574be01f808902b0c1de6b22bf4
YanisAoudia <yanis.aoudia@ensea.fr>
2025-12-11 16:40:35 +0100

    Q1 enseash.c
    This function displays a welcome message and a prompt for the user, then enters an infinite loop where it displays the prompt and reads what the user types on the keyboard. It stores the input in an array, but does not yet process the command.

--- enseash.c
+++ enseash.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
+#define INPUT_BUFSIZE 128
+
+
+const char welcome1[]="Bienvenue dans le Shell ENSEA.\n";
+const char welcome2[]="Pour quitter, tapez 'exit'.\n";
+const char prompt[]="enseash %";
+const char exit_1[]="exit";
+
+void printscript(const char *script){
+    write(STDOUT_FILENO, script, strlen(script));
+}
+  
+void enseash(void){
+    char stock[INPUT_BUFSIZE];
+    ssize_t nread;
+
+    printscript(welcome1);
+    printscript(welcome2);
+
+    while(1){
+        printscript(prompt);        
+        nread=read(STDOUT_FILENO, stock, INPUT_BUFSIZE-1);
+
+        if (nread <= 0){
+            break;
+        } // Stops the loop if reading input fails or reaches end of file
+    }
+}
+
+int main(){
+    enseash();
+    return 0;
+}
